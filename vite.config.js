9b69adce87bee2de9e377feeedb2645403926180
yasmnask <zakiyahyasmin1@gmail.com>
2025-08-11 15:58:09 +0700

    deploy v5

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     base: "/",
+    build: {
+        outDir: "public/build",
+    },
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
