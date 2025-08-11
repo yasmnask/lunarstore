30144399f04fbb48938724637007059afd7a8ea2
yasmnask <zakiyahyasmin1@gmail.com>
2025-08-11 18:26:50 +0700

    deploy v8

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->prepend(\Illuminate\Http\Middleware\HandleCors::class);
 
         //
     })
